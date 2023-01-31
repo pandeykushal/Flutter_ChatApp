306484137161e0003e64f7d07d061e10481e1ada
KUSHAL PANDEY <kUSHAL.PANDEY118@GMAIL.COM>
2023-01-31 14:13:02 +0545

    firebase

---
+++ lib/constant/constant.dart
@@ -0,0 +1,7 @@
+class constants{
+static String appId="1:716032681719:web:d5e661b98264fd0b6915f2";
+ static String apiKey="AIzaSyDkG7-ls9kpQ91cA0Wt4_uxJCgrX-lY1Bc" ;
+static  String projectId="chatapp-fe049";
+static  String messagingSenderId="716032681719";
+}
+  
